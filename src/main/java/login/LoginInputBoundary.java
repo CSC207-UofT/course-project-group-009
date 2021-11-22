3d3e0d2f70b13fd27cf9d5bfd9c3f9be02b504dc
Yiyang Du <yiyang.du@mail.utoronto.ca>
2021-11-21 21:08:02 -0500

    basic structures for LoginSystem, having boundary for Administrators to login with administrator codes

--- src/main/java/login/LoginInputBoundary.java
+++ src/main/java/login/LoginInputBoundary.java
@@ -1,4 +1,7 @@
 package login;
 
 public interface LoginInputBoundary {
+    LoginUseCase.LoginResult logIn(String username, String password);
+
+    LoginUseCase.LoginResult logIn(String administratorcode);
 }
--- src/main/java/login/LoginUseCase.java
+++ src/main/java/login/LoginUseCase.java
@@ -1,4 +1,35 @@
 package login;
 
-public class LoginUseCase {
+import java.io.IOException;
+
+
+public abstract class LoginUseCase implements LoginInputBoundary {
+
+    private final UserList users;
+
+
+    UserReadWriter areadWriter = new UserReadWriter();
+
+
+    public enum AttemptResult {
+        SUCCESS, FAILURE
+    }
+
+    public LoginUseCase(UserList users){
+        this.users = users;
+        try {
+            areadWriter.saveToFile("user.ser", users);
+        } catch (IOException ae) {
+            System.out.println("not saved.");
+        }
+    }
+
+    public LoginResult logIn(String username, String passwprd) {
+        User user = users.getUser(username);
+        if (user.passwordMatches(password)){
+            return LoginResult.SUCCESS;
+        }else{
+            return LoginResult.FAILURE;
+        }
+    }
 }
